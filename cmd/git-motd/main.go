e24e8a54494e8a3c330567afd9a0f8b9a53746ef
Sean Enck <sean@ttypty.com>
2024-10-21 18:37:05 -0400

    minor motd speedup

--- cmd/git-motd/main.go
+++ cmd/git-motd/main.go
@@ -2,10 +2,13 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
 	"os"
+	"slices"
+	"strings"
 
 	"github.com/seanenck/git-tools/internal/cli"
 	"github.com/seanenck/git-tools/internal/dotfiles"
@@ -13,18 +16,19 @@ import (
 )
 
 type cmd struct {
-	written bool
 	name    string
+	written bool
 	fxn     func(io.Writer) error
+	buf     *bytes.Buffer
 }
 
 func (c *cmd) Write(data []byte) (int, error) {
 	if !c.written {
-		fmt.Printf("[%s]\n", c.name)
-		fmt.Println("===")
+		fmt.Fprintf(c.buf, "[%s]\n", c.name)
+		fmt.Fprintln(c.buf, "===")
 		c.written = true
 	}
-	fmt.Println(string(data))
+	fmt.Fprintln(c.buf, string(data))
 	return len(data), nil
 }
 
@@ -49,13 +53,47 @@ func run() error {
 	if len(os.Args) != 1 {
 		return errors.New("no arguments allowed")
 	}
+	type result struct {
+		name string
+		err  error
+		res  []byte
+	}
+	var all []chan result
 	for _, c := range []*cmd{
 		{name: "dotfiles", fxn: dotfileDiff},
 		{name: "uncommitted", fxn: uncommit},
 	} {
-		if err := c.fxn(c); err != nil {
-			return err
+		r := make(chan result)
+		c.buf = &bytes.Buffer{}
+		go func(command *cmd, res chan result) {
+			obj := result{name: command.name}
+			if err := command.fxn(command); err == nil {
+				obj.res = command.buf.Bytes()
+			} else {
+				obj.err = err
+			}
+			res <- obj
+		}(c, r)
+		all = append(all, r)
+	}
+	var collected []result
+	var errs []error
+	for _, a := range all {
+		res := <-a
+		if res.err != nil {
+			errs = append(errs, res.err)
+			continue
 		}
+		collected = append(collected, res)
+	}
+	if len(errs) > 0 {
+		return errors.Join(errs...)
+	}
+	slices.SortFunc(collected, func(a, b result) int {
+		return strings.Compare(a.name, b.name)
+	})
+	for _, item := range collected {
+		fmt.Print(string(item.res))
 	}
 	return nil
 }
